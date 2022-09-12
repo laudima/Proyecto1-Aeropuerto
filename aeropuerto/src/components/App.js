0b7bb7a218d39942a10b932f1f63afc4ae2f3b40
laudima <laudima@ciencias.unam.mx>
2022-09-11 23:37:57 -0500

    ya se puede escribir el hello world

---
+++ aeropuerto/src/components/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App(){
+    return (<h1> Aeropuerto </h1>);
+}
+
+export default App;
--- aeropuerto/src/index.js
+++ aeropuerto/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./componentes/App";
+import App from './componentes/App';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
