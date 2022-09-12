d273af1eb8c389b7426277c026dad84b045e6644
Alex <alexbri@ciencias.unam.mx>
2022-09-12 02:33:12 -0400

    Hice la estructura de la seccion general con datos de ejemplo y agregue la prueba unitaria de renderizado del componente Datos

--- aeropuerto/src/componentes/seccion_general/Logo.js
+++ aeropuerto/src/componentes/seccion_general/Logo.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 function Logo(){
-    return <div></div>;
+    return (
+            <div>
+
+            <p style={{fontSize:"28px", fontWeight:"bold"}}>Aerolínea Carrillo</p>
+
+            </div>);
 }
 
 export default Logo;
