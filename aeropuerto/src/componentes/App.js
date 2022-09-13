956436c61afb7e79920228a67f5e182308285945
Alex <alexbri@ciencias.unam.mx>
2022-09-12 20:09:14 -0400

    Ajuste las posiciones de los datos generales y agregue hooks para que se muestren los datos recibidos de la api

--- aeropuerto/src/componentes/App.js
+++ aeropuerto/src/componentes/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import {React, useState} from "react";
 import Nubes from "../imagenes/nubes1.jpg" // Imagen provisional
 import DatosGenerales from "./seccion_general/DatosGenerales";
 import Columna from "./columna /Columna";
@@ -12,21 +12,30 @@ import Config from "../config.js"
 function App() {
 
   const llave = Config.llave;
-  console.log(llave);
-  
-  let URL = "https://api.checkwx.com/metar/lat/40.72/lon/-73.99/decoded";
+  const [ciudad, setCiudad] = useState("Monterrey");
+  const [temperatura, setTemperatura] = useState("16");
+  /*
+  let URL = "https://api.checkwx.com/metar/lat/25.77/lon/-100.10/decoded";
   fetch(URL, {
     method: "GET",
     headers: {"X-API-Key": llave}
   })
     .then(response => response.json())
-    .then(data => console.log(data));
-  
+    .then(datos =>  {
+      setTemperatura(datos.data[0].temperature.celsius.toString());
+      console.log(datos.data[0].temperature.celsius);
+    });
+    */
   return (
   
     <div className="app" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${Nubes})`, backgroundSize:'cover'}}>
       
-      <div className="columna-datos-generales"><DatosGenerales /></div>
+      <div className="columna-datos-generales">
+        <DatosGenerales
+          temperatura={temperatura}
+          ciudad={ciudad}
+          />
+      </div>
       <div className="columna-detalles"><Columna /></div>
     </div>
   );
