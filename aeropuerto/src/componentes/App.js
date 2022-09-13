13243851b4bad70a3790f7b6e6a18048a9cae80a
laudima <laudima@ciencias.unam.mx>
2022-09-13 02:50:22 -0500

    cambio para uqe compile

--- aeropuerto/src/componentes/App.js
+++ aeropuerto/src/componentes/App.js
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, {useState} from "react";
 =======
 import {{React, useState}, useState} from "react";
 >>>>>>> b5e5455c (Ajuste las posiciones de los datos generales y agregue hooks para que se muestren los datos recibidos de la api)
+=======
+import React, {useState} from "react";
+>>>>>>> 297780b1 (cambio para uqe compile)
 import Nubes from "../imagenes/nubes1.jpg" // Imagen provisional
 import DatosGenerales from "./seccion_general/DatosGenerales";
 import Columna from "./columna /Columna";
