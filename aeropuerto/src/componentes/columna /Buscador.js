dcd18c0f826ad41611a37dd409b392547ef7f1e9
laudima <laudima@ciencias.unam.mx>
2022-09-13 02:03:46 -0500

    ya esta la columana y el boton

--- aeropuerto/src/componentes/columna /Buscador.js
+++ aeropuerto/src/componentes/columna /Buscador.js
@@ -1,10 +1,16 @@
 import React from "react";
-
-
+import iconoBuscar from "../../imagenes/search-i.svg";
 function Buscador(props) {
     return (
-        <div className="buscador"> 
-            <input type="text" placeholder="Buscar" />
+        <div className="search">
+            <div className="searchInputs">
+                <input type="text" placeholder="Buscar ciudad" />
+                <div className="searchIcon">
+                    <img style={{width:"20px", height:"20px"}} src={iconoBuscar} alt="icono_buscar" />
+                </div>
+            </div>
+            <div className="searchResults"> //falta agregar los resultados de la busqueda   
+            </div>
         </div>
     );
 }
--- aeropuerto/src/componentes/columna /OtrasCiudades.js
+++ aeropuerto/src/componentes/columna /OtrasCiudades.js
@@ -4,10 +4,13 @@ import TableroDetalles from "./TableroDetalles";
 
 function OtrasCiudades(props){
     return <TableroDetalles
-        titulo="Otras ciudades"
+        titulo="Otras Ciudades"
         dato1= "Ciudad de Mexico"
+        dato1info= {props.ciudad1}
         dato2="Guadalajara"
+        dato2info= {props.ciudad2}
         dato3= "Monterrey"
+        dato3info= {props.ciudad3}
     />
 }
 
